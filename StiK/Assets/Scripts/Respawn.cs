6f6204ba37daba9d0ea5bcf8f88c1c35afc97dc8
Nicholas Tidwell <nicktidwell52@gmail.com>
2019-10-21 13:52:40 -0400

    Added Respawning
    Added Script to Respawn player after falling off map.

---
+++ StiK/Assets/Scripts/Respawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public float respawnDistance;
+    public Vector2 respawnPoint;
+    void FixedUpdate()
+    {
+        if (transform.position.y < respawnDistance)
+            transform.position = respawnPoint;
+    }
+}
